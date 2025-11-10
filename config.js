f947deabafd337a27fd60796178ca139ea585fa5
Goladipo Web <goladipowebsite@gmail.com>
2025-11-10 11:40:46 +0100

    11 commit

---
+++ config.js
@@ -0,0 +1,7 @@
+// Local config - NOT tracked by git. Keep this file private.
+// If you rotate keys, update these values accordingly.
+window.__CONFIG__ = {
+	SUPABASE_URL: 'https://xowxltikqnhhvftzoasa.supabase.co',
+	SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhvd3hsdGlrcW5oaHZmdHpvYXNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3MTU3NTksImV4cCI6MjA3NzI5MTc1OX0.8pE_CVCua7b3i3mkM9UM6WXBmlrB907YV0_Qiul-jU8'
+};
+
